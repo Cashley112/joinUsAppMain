8f370bf3d1059f3a131567bb50679719df3e0d60
Connor Ashley <connorashley@Connors-MacBook-Pro.local>
2021-03-15 22:31:53 -0400

    .

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/joke", (req, res) => res.send("Your life"));
 
 app.listen(3000, () => console.log("Server listening on port 3000!"));
